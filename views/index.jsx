2be3e52fc30e242ab60f290e572ef3bdb62c2b84
Ryan Magoon <mr.magoon5@gmail.com>
2016-09-06 16:29:02 -0400

    challenge 4 completed

--- views/index.jsx
+++ views/index.jsx
@@ -20,7 +20,8 @@ class TodoList extends React.Component {
         <table style={{ border: '2px solid black' }}>
           <tbody>
             <Todo title="Shopping">Milk</Todo>
-            <Todo title="Hair cut">13: 00</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+            <Todo title="Learn React">15:00</Todo>
           </tbody>
         </table>
       </div>
@@ -38,6 +39,10 @@ class Todo extends React.Component {
     );
   }
 }
+
+Todo.propTypes = {
+    title: React.PropTypes.string.isRequired
+  }
 
 class TodoForm extends React.Component {
   render() {
