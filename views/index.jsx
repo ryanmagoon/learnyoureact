144c90fd2fdaabb37ade1e97a3fa606bd6fd04f5
Ryan Magoon <mr.magoon5@gmail.com>
2016-09-08 16:21:48 -0400

    finished exercise 7

--- views/index.jsx
+++ views/index.jsx
@@ -4,30 +4,29 @@ import ReactDOM from 'react-dom';
 export default class TodoBox extends React.Component {
   render() {
     return (
-      <div className="todoBox">
-        <h1>Todos</h1>
-        <TodoList />
-        <TodoForm />
-      </div>
-    );
+          <div className="todoBox">
+            <h1>Todos</h1>
+            <TodoList data = {this.props.data} />
+            <TodoForm />
+          </div>
+        );
   }
-}
+    }
 
 class TodoList extends React.Component {
   render() {
+    var todo = this.props.data.map(function(obj) { return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>;});
     return (
-      <div className="todoList">
-        <table style={{ border: '2px solid black' }}>
-          <tbody>
-            <Todo title="Shopping">Milk</Todo>
-            <Todo title="Hair cut">13:00</Todo>
-            <Todo title="Learn React">15:00</Todo>
-          </tbody>
-        </table>
-      </div>
-    );
+          <div className = "todoList">
+            <table style={{border: '2px solid black'}}>
+              <tbody>
+                {todo}
+              </tbody>
+            </table>
+          </div>
+        );
   }
-}
+    }
 
 let Todo = React.createClass({
   getInitialState() {
